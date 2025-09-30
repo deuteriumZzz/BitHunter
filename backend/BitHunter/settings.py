57b9a6cbeb0b59bc9a2531d99c5cde828fb02564
Dmitry Vologdin <deuteriumzzz@gmail.com>
2025-09-30 09:48:52 +0800

    Add apps

--- backend/BitHunter/settings.py
+++ backend/BitHunter/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'alerts',
+    'analytics',
+    'api',
+    'trading',
 ]
 
 MIDDLEWARE = [
